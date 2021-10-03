7f32c37148e0b338c89d0930c829220470276354
Bois <boisathell@gmail.com>
2021-10-03 17:34:54 -0500

    prob and accel

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -935,7 +935,7 @@
    Override with M201
                                         X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
 */
-#define DEFAULT_MAX_ACCELERATION      {9000,9000,100, 10000} 
+#define DEFAULT_MAX_ACCELERATION      {3000,3000,100, 10000} 
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
@@ -950,7 +950,7 @@
      M204 R    Retract Acceleration
      M204 T    Travel Acceleration
 */
-#define DEFAULT_ACCELERATION          3000    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_ACCELERATION          2000    // X, Y, Z and E acceleration for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration for retracts
 #define DEFAULT_TRAVEL_ACCELERATION   3000    // X, Y, Z acceleration for travel (non printing) moves
 
@@ -1167,7 +1167,7 @@
        |    [-]    |
        O-- FRONT --+
 */
-#define NOZZLE_TO_PROBE_OFFSET { 26.7, 0, -0.8 }
+#define NOZZLE_TO_PROBE_OFFSET { 26.7, 0, -1.3 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.

9fc8606c52e1c18532851b4d24ed8c50ac0a3623
Bois <boisathell@gmail.com>
2021-10-18 21:55:45 -0500

    default probe speed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1185,7 +1185,7 @@
 #define Z_PROBE_FEEDRATE_FAST (4*60)
 
 // Feedrate (mm/min) for the "accurate" probe of each point
-#define Z_PROBE_FEEDRATE_SLOW (Z_PROBE_FEEDRATE_FAST / 4)
+#define Z_PROBE_FEEDRATE_SLOW (Z_PROBE_FEEDRATE_FAST / 2)
 
 /**
    Probe Activation Switch

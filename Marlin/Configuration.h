9f82148ed250bdc9ddb827b922b0c06274f7ebcf
Bois <boisathell@gmail.com>
2021-10-20 21:11:05 -0500

    bed size temp decrease

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1362,7 +1362,7 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 260
+#define X_BED_SIZE 200
 #define Y_BED_SIZE 298
 
 // Travel limits (mm) after homing, corresponding to endstop positions.

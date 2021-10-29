05566752e90f15ea3ee6f88dbc92852cb697a8bd
Bois <boisathell@gmail.com>
2021-10-29 08:44:16 -0500

    25 probe

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1597,7 +1597,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
 // Set the number of grid points per dimension.
-#define GRID_MAX_POINTS_X 10
+#define GRID_MAX_POINTS_X 25
 #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
 // Probe along the Y axis, advancing X after each column

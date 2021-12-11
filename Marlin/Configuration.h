88013fc7bd5f5d683b1de9ef216589a2ec1a4472
An-GG <ankush.girotra@gmail.com>
2021-12-11 04:53:31 -0600

    disable corners

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1629,7 +1629,7 @@
 
 #define MESH_EDIT_GFX_OVERLAY   // Display a graphics overlay while editing the mesh
 
-#define MESH_INSET 20             // Set Mesh bounds as an inset region of the bed
+#define MESH_INSET 10             // Set Mesh bounds as an inset region of the bed
 #define GRID_MAX_POINTS_X 12      // Don't use more than 15 points per axis, implementation limited.
 #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
@@ -1664,13 +1664,13 @@
 #define LCD_BED_LEVELING
 
 #if ENABLED(LCD_BED_LEVELING)
-#define MESH_EDIT_Z_STEP  0.025 // (mm) Step size while manually probing Z axis.
+#define MESH_EDIT_Z_STEP  0.01 // (mm) Step size while manually probing Z axis.
 #define LCD_PROBE_Z_RANGE 4     // (mm) Z Range centered on Z_MIN_POS for LCD Z adjustment
 #define MESH_EDIT_MENU        // Add a menu to edit mesh points
 #endif
 
 // Add a menu item to move between bed corners for manual bed adjustment
-#define LEVEL_BED_CORNERS
+//#define LEVEL_BED_CORNERS
 
 #if ENABLED(LEVEL_BED_CORNERS)
 #define LEVEL_CORNERS_INSET_LFRB { 30, 30, 30, 30 } // (mm) Left, Front, Right, Back insets

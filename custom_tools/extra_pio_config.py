3364c977df0f2b9fd3221854ebf00d6f49d75313
An-GG <ankush.girotra@gmail.com>
2021-12-11 06:32:17 -0600

    error on upload script

--- custom_tools/extra_pio_config.py
+++ custom_tools/extra_pio_config.py
@@ -16,7 +16,7 @@ def on_upload(source, target, env):
             port=str(next(possible).device)
         except (StopIteration):
             sys.exit("\nERROR: Automatic port search failed. Specifiy a port manually with '--upload-port'")
-    env.Execute("custom_tools/skr_uploader/upload.py " + port + " 250000 " + firmware_path)
-    return 0
+    
+    return env.Execute("custom_tools/skr_uploader/upload.py " + port + " 250000 " + firmware_path)
 
 env.Replace(UPLOADCMD=on_upload)

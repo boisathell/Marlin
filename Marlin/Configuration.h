cccf5975c89663cc75fd425a38b8867cd49f8f8e
An-GG <ankush.girotra@gmail.com>
2021-12-11 04:42:18 -0600

    inset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1629,11 +1629,11 @@
 
 #define MESH_EDIT_GFX_OVERLAY   // Display a graphics overlay while editing the mesh
 
-#define MESH_INSET 10             // Set Mesh bounds as an inset region of the bed
+#define MESH_INSET 20             // Set Mesh bounds as an inset region of the bed
 #define GRID_MAX_POINTS_X 12      // Don't use more than 15 points per axis, implementation limited.
 #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
-//#define UBL_HILBERT_CURVE       // Use Hilbert distribution for less travel when probing multiple points
+#define UBL_HILBERT_CURVE       // Use Hilbert distribution for less travel when probing multiple points
 
 #define UBL_MESH_EDIT_MOVES_Z     // Sophisticated users prefer no movement of nozzle
 #define UBL_SAVE_ACTIVE_ON_M500   // Save the currently active mesh in the current slot on M500
